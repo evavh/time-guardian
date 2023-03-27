e419f8cfe1d5ee70d4601de5b5a4ae5b42ae1b91
Eva van Houten <opensource@evavh.net>
2023-03-27 12:22:10 +0200

    Change to skeleton for custom config, add test

--- src/config.rs
+++ src/config.rs
@@ -1,31 +1,117 @@
-use serde_derive::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::fs;
+use std::path::Path;
 
-const APP_NAME: &str = "time-guardian";
-const CONFIG_NAME: &str = "config";
+use chrono::{Duration, NaiveTime, Weekday};
 
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
-pub(crate) struct Config {
-    pub(crate) allowed_times: Vec<(String, String)>,
+const DEFAULT_CONFIG: &str = "<user>
+   Monday
+      00:00-24:00 24h
+   Tuesday
+      00:00-24:00 24h
+   Wednesday
+      00:00-24:00 24h
+   Thursday
+      00:00-24:00 24h
+   Friday
+      00:00-24:00 24h
+   Saturday
+      00:00-24:00 24h
+   Sunday
+      00:00-24:00 24h
+";
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+struct TimeRange {
+    start: NaiveTime,
+    end: NaiveTime,
 }
 
-impl std::default::Default for Config {
-    fn default() -> Self {
-        Self {
-            allowed_times: vec![("00:00".to_owned(), "24:00".to_owned())],
+#[derive(Debug, PartialEq, Eq, Clone)]
+struct Timeslot {
+    range: TimeRange,
+    duration: Duration,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+struct DayConfig {
+    total_duration: Duration,
+    timeslots: Vec<Timeslot>,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub(crate) struct Config {
+    users: HashMap<String, HashMap<Weekday, DayConfig>>,
+}
+
+impl Config {
+    fn from_string(file_contents: String) -> Result<Config, std::io::Error> {
+        todo!()
+    }
+
+    fn load(path: &Path) -> Result<Config, std::io::Error> {
+        let file_contents = String::from_utf8(fs::read(path)?).unwrap();
+        Config::from_string(file_contents)
+    }
+
+    pub(crate) fn initialize(path: &Path) -> Config {
+        match Config::load(path) {
+            Ok(config) => config,
+            Err(e) if e.kind() == std::io::ErrorKind::NotFound => {
+                fs::write(path, DEFAULT_CONFIG).unwrap();
+                println!(
+                    "No config found, writing default config to {}.",
+                    path.display()
+                );
+                Config::load(path)
+                    .expect("The config file should exist and be valid now.")
+            }
+            Err(e) => panic!("Unexpected IO error: {e}"),
         }
     }
+
+    pub(crate) fn default() -> Config {
+        todo!()
+    }
 }
 
-pub(crate) fn initialize() -> Config {
-    // confy::store(APP_NAME, CONFIG_NAME, Config::default()).unwrap();
-    let config: Config = confy::load(APP_NAME, CONFIG_NAME).unwrap();
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    if config == Config::default() {
-        let path =
-            confy::get_configuration_file_path(APP_NAME, CONFIG_NAME).unwrap();
-        println!("No user configuration found.");
-        println!("The config file can be found at {}", path.display());
+    #[test]
+    fn parse_default_config() {
+        use chrono::Weekday::*;
+
+        let parsed = Config::from_string(DEFAULT_CONFIG.to_owned());
+        let total_duration = Duration::hours(24);
+
+        let start = NaiveTime::from_hms_opt(0, 0, 0).unwrap();
+        let end = NaiveTime::from_hms_opt(24, 0, 0).unwrap();
+
+        let range = TimeRange { start, end };
+        let timeslot = Timeslot {
+            range,
+            duration: total_duration,
+        };
+        let timeslots = vec![timeslot];
+
+        let dayconfig = DayConfig {
+            total_duration,
+            timeslots,
+        };
+        let days = HashMap::from([
+            (Mon, dayconfig.clone()),
+            (Tue, dayconfig.clone()),
+            (Wed, dayconfig.clone()),
+            (Thu, dayconfig.clone()),
+            (Fri, dayconfig.clone()),
+            (Sat, dayconfig.clone()),
+            (Sun, dayconfig.clone()),
+        ]);
+        let users = HashMap::from([("<user>".to_owned(), days)]);
+        let correct = Config { users };
+
+        assert_eq!(parsed.unwrap(), correct);
     }
-
-    config
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
+use std::path::Path;
+
 use chrono::Local;
+
+use config::Config;
 
 mod config;
 
+const CONFIG_PATH: &str = "/home/focus/.config/time-guardian/config";
+
 fn main() {
-    let config = config::initialize();
+    let config = Config::initialize(Path::new(CONFIG_PATH));
+    if config == Config::default() {
+        println!("Default config found, please edit at {CONFIG_PATH}.");
+    }
     dbg!(config);
 
     let now = Local::now();
