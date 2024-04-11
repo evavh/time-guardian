2f14e35b68090ff36bb44ff18021c86044bcb113
Eva van Houten <opensource@evavh.net>
2024-04-11 11:50:46 +0200

    Delete config parsing (for now, first simplifying)

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,9 @@ use std::path::Path;
 
 use chrono::Local;
 
-use config::Config;
-
-mod config;
-
 const CONFIG_PATH: &str = "/home/focus/.config/time-guardian/config";
 
 fn main() {
-    let config = Config::initialize(Path::new(CONFIG_PATH));
-    if config == Config::default() {
-        println!("Default config found, please edit at {CONFIG_PATH}.");
-    }
-    dbg!(config);
-
     let now = Local::now();
     dbg!(now);
 }
