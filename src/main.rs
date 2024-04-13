c3085173ebae496f056634a3f331f26b34b692f5
Eva van Houten <opensource@evavh.net>
2024-04-13 16:08:07 +0200

    Create function is_active for user

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
-use std::{collections::HashMap, path::Path};
-
-use chrono::Local;
+use std::{collections::HashMap, process::Command};
 
 const CONFIG_PATH: &str = "/home/focus/.config/time-guardian/config";
 
 fn main() {
     let config = std::fs::read_to_string(CONFIG_PATH).unwrap();
 
-    let entries: HashMap<&str, usize> = config
+    let settings: HashMap<&str, usize> = config
         .lines()
         .map(|line| line.split(","))
         .map(|mut entry| {
@@ -18,8 +16,25 @@ fn main() {
         })
         .collect();
 
-    dbg!(entries);
+    dbg!(is_active("focus"));
+}
 
-    let now = Local::now();
-    dbg!(now);
+fn is_active(user: &str) -> bool {
+    let output = Command::new("loginctl")
+        .arg("show-user")
+        .arg(user)
+        .arg("--property=State")
+        .output()
+        .unwrap();
+
+    let err = std::str::from_utf8(&output.stderr).unwrap();
+    if !err.is_empty() {
+        assert!(
+            err.contains("is not logged in or lingering"),
+            "Unknown loginctl error, output: {output:?}"
+        );
+    }
+    let state = std::str::from_utf8(&output.stdout).unwrap();
+
+    state.contains("active")
 }
