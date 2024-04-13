e53ec2d9cae1c57a9ac88633859cb1f4e96ba638
Eva van Houten <opensource@evavh.net>
2024-04-13 15:48:01 +0200

    Create new minimal config parsing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
-use std::path::Path;
+use std::{collections::HashMap, path::Path};
 
 use chrono::Local;
 
 const CONFIG_PATH: &str = "/home/focus/.config/time-guardian/config";
 
 fn main() {
+    let config = std::fs::read_to_string(CONFIG_PATH).unwrap();
+
+    let entries: HashMap<&str, usize> = config
+        .lines()
+        .map(|line| line.split(","))
+        .map(|mut entry| {
+            (
+                entry.next().unwrap(),
+                entry.next().unwrap().parse::<usize>().unwrap(),
+            )
+        })
+        .collect();
+
+    dbg!(entries);
+
     let now = Local::now();
     dbg!(now);
 }
