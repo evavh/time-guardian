93d36ca15c1fd82fa4448488fa5e769baf30d81e
Eva van Houten <opensource@evavh.net>
2024-05-10 15:10:32 +0200

    Apply code review changes (error handling)

--- src/notification.rs
+++ src/notification.rs
@@ -1,12 +1,18 @@
 use std::process::Command;
+use std::string::FromUtf8Error;
+use std::sync::Arc;
 
 use color_eyre::{eyre::Context, Result};
 use thiserror::Error;
 
 #[derive(Error, Debug, Clone)]
-pub enum Error {
+enum Error {
     #[error("Couldn't parse loginctl output: {0}")]
-    ParseLoginctlOutput(String),
+    Parse(String),
+    #[error("Io error")]
+    Io(#[from] Arc<std::io::Error>),
+    #[error("Utf8 parsing error")]
+    Utf8(#[from] FromUtf8Error),
 }
 
 pub(crate) fn notify_user(target_name: &str, text: &str) {
@@ -17,23 +23,12 @@ pub(crate) fn notify_user(target_name: &str, text: &str) {
 }
 
 fn notify_user_err(target_name: &str, text: &str) -> Result<()> {
-    let users =
-        get_logged_in_users().wrap_err("Couldn't get logged in users")?;
+    let users = get_logged_in_users().wrap_err("Couldn't get logged in users")?;
 
     let user = users
         .iter()
         .cloned()
-        .filter(|x| x.is_ok())
-        .map(|x| x.expect("Filtered on ok"))
         .find(|(_uid, name)| name == target_name);
-
-    let first_error = users.iter().cloned().filter(|x| x.is_err()).next();
-    match first_error {
-        Some(Err(err)) => return Err(err).wrap_err("Error while parsing loginctl"),
-        None => (),
-        Some(Ok(_)) => unreachable!(),
-
-    };
 
     match user {
         Some((uid, name)) => notify(&name, &uid, text),
@@ -44,24 +39,25 @@ fn notify_user_err(target_name: &str, text: &str) -> Result<()> {
     Ok(())
 }
 
-fn get_logged_in_users() -> Result<Vec<Result<(String, String), Error>>> {
-    let users = Command::new("loginctl").output()?.stdout;
+fn get_logged_in_users() -> Result<Vec<(String, String)>, Error> {
+    let users = Command::new("loginctl").output().map_err(Arc::new)?.stdout;
     let users = String::from_utf8(users)?;
-    Ok(users
+    let users: Result<Vec<(String, String)>, _> = users
         .lines()
         .filter(|x| x.starts_with(' '))
         .map(|x| x.split(' ').filter(|x| !x.is_empty()))
         .map(|mut x| {
             Ok((
                 x.nth(1)
-                    .ok_or(Error::ParseLoginctlOutput(x.clone().collect()))?
+                    .ok_or(Error::Parse(x.clone().collect()))?
                     .to_string(),
                 x.next()
-                    .ok_or(Error::ParseLoginctlOutput(x.collect()))?
+                    .ok_or(Error::Parse(x.collect()))?
                     .to_string(),
             ))
         })
-        .collect())
+        .collect();
+    users
 }
 
 fn notify(username: &str, uid: &str, text: &str) {
